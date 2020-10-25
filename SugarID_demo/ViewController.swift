24da58a0e72cecc68a73f568ee44cf60be6bd27a
Zeke <zrong7@wisc.edu>
2020-10-25 19:15:08 +0800

    try

--- SugarID_demo/ViewController.swift
+++ SugarID_demo/ViewController.swift
@@ -8,13 +8,70 @@
 
 import UIKit
 
+
+
+
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var textInput: UITextField!
+    
+    @IBOutlet weak var output: UILabel!
+    
+    var bloodSugar = 0.0
+    var insulin = 0.0
+    var food = 0.0
+    var carbRatio = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
 
+
+    @IBAction func bloodSugar(_ sender: UIButton, forEvent event: UIEvent) {
+        bloodSugar = Double(textInput!.text!) ?? 0
+        print("bloodSugar\(bloodSugar)")
+        
+        //check if all required data has been filled in
+        if bloodSugar <= 0{
+            output.text = String("Entered wrong blood sugar value, please try again :)")
+        }
+        
+    }
+    
+    @IBAction func insulin(_ sender: UIButton, forEvent event: UIEvent) {
+        insulin = Double(textInput!.text!) ?? 0
+        print("insulin\(insulin)")
+        
+        //check if all required data has been filled in
+        if bloodSugar == 0 || insulin == 0 || food == 0 || carbRatio == 0{
+            output.text = String("need more data :)")
+        }
+    }
+    
+    @IBAction func food(_ sender: UIButton, forEvent event: UIEvent) {
+        food = Double(textInput!.text!) ?? 0
+        print("food\(food)")
+    }
+    
+    @IBAction func carbRatio(_ sender: Any) {
+        carbRatio = Double(textInput!.text!) ?? 0
+        print("carbRatio\(carbRatio)")
+        if carbRatio <= 0{
+            output.text = String("Wrong Carb Ratio Entered, please entered it again :)")
+        }
+        else if carbRatio > 0{
+            output.text = String("Carb Ratio Entered! :)")
+        }
+    }
+    
+    
+    
+    
+    
+    
 
 }
 

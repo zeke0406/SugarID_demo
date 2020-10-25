782403ab18929de2f41a511595df4de9cd71ea4e
Zeke <zrong7@wisc.edu>
2020-10-25 17:07:34 +0800

    Initial Commit

---
+++ SugarID_demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SugarID_demo
+//
+//  Created by Aries Rong on 10/25/20.
+//  Copyright © 2020 Zeke Rong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
